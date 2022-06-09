052370043f6e11df5ea638be2602060bb620ba8e
voronrk <voron_76@mail.ru>
2022-06-09 13:32:03 +0300

    Fix bug in the Config.example

---
+++ Config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Config;
+
+class Config
+{
+    public static $baseURI = 'http://www.cbr-xml-daily.ru';
+    public static $method = '/daily_json.js';
+    public static $tlgUri = 'https://api.telegram.org/bot';
+    public static $tlgKey = '';
+}
